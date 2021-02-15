7a47d698a6a73258e4390e2939c443cb36e93a3f
WataruTomita <tomi.symphonia@gmail.com>
2021-02-15 19:18:42 +0900

    db default

--- db/migrate/20210215092755_create_orders.rb
+++ db/migrate/20210215092755_create_orders.rb
@@ -5,10 +5,10 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.integer :delivery_zipcode
       t.integer :delivery_address
       t.string :delivery_name
-      t.integer :shipping_price
-      t.integer :billing_amount
-      t.integer :payment_method
-      t.integer :order_status
+      t.integer :shipping_price, default: 800  # 送料
+      t.integer :billing_amount, default: 0    # 請求額
+      t.integer :payment_method, defalut: 0    # 支払方法
+      t.integer :order_status    # 注文状況status
 
       t.timestamps
     end
