55a8539119b933c9229d281e5e276c32a953ca1e
WataruTomita <tomi.symphonia@gmail.com>
2021-02-16 11:42:32 +0900

    order.rb created

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :customer
   has_many :order_products, dependent: :destroy
 
@@ -13,6 +12,4 @@ class Order < ApplicationRecord
   
   enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
   enum order_status: {"入金待ち": 0, "入金確認": 1, "制作中": 2, "発送準備中": 3, "発送済み": 4 }
-=======
->>>>>>> 03fa423b8822b8caa702cd72d3c50cd9642c05d4
 end
