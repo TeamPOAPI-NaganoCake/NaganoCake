cc570d37be1f2c76ead66c9259a54288d9041326
adoll65140 <vattirii@gmail.com>
2021-02-16 02:09:02 +0000

    create_customer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  protect_from_forgery with: :null_session
+  
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana,
+                                                       :zip_code, :address, :phone_number])
   end
 end
