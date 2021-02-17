40f819ca4a1cf86512bc8bbbfa110ba88287f37f
adoll65140 <vattirii@gmail.com>
2021-02-17 02:02:47 +0000

    create_customer_delivery

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,36 +1,45 @@
 class CustomersController < ApplicationController
+  
+  before_action :authenticate_customer!
+  before_action :set_customer
+
+
   def show
-    @customer = Customer.find(params[:id])
   end
 
   def edit
-    @customer = Customer.find(params[:id])
   end
 
   def update
-     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to customer_path(@customer)
+      redirect_to customers_path
     else
       render "edit"
     end
   end
 
   def unsubscribe
-     @customer = Customer.find(params[:id])
   end
 
   def withdrew
-    @customer = Customer.find(params[:id])
-    @customer.update(cancel_flag: true)
+    customer = current_customer
+    customer.update(cancel_flag: false)
     reset_session
     redirect_to root_path
   end
-
+  
   private
+
+  
+  
+  def set_customer
+    @customer = current_customer
+  end
 
   def customer_params
     params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
                                                        :zip_code, :address, :phone_number)
   end
+  
+  
 end
