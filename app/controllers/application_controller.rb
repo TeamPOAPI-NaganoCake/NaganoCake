10b87f06fc8a72740965d9f509e79385a54fcdb3
adoll65140 <vattirii@gmail.com>
2021-02-17 23:06:11 +0000

    change_root/add_order_item_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session
   
+ 
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana,
+                                                       :zip_code, :address, :phone_number])
+  end
+
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
       admins_homes_top_path
@@ -10,10 +19,4 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana,
-                                                       :zip_code, :address, :phone_number])
-  end
 end
