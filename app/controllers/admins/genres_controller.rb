72f591da35c3ae84e00a6a06a5fda6c08b4c63a5
adoll65140 <vattirii@gmail.com>
2021-02-18 22:04:12 +0000

    genre

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,7 +1,42 @@
 class Admins::GenresController < ApplicationController
+  before_action :set_genre, only: [:edit, :update]
+  
   def index
+    @genre = Genre.new
+    @genres = Genre.all.page(params[:page]).per(10)
   end
 
   def edit
+  end
+
+
+  def create
+  	@genre = Genre.new(genre_params)
+    if @genre.save
+       flash[:notice] = "ジャンルを追加しました"
+       redirect_to admins_genres_path
+    else
+      @genres = Genre.all.page(params[:page]).per(10)
+      render :index
+    end
+  end
+  
+  def update
+    if @genre.update(genre_params)
+      flash[:success] = "ジャンルを変更しました"
+      redirect_to admins_genres_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def genre_params
+    params.require(:genre).permit(:genre_name)
+  end
+
+  def set_genre
+    @genre = Genre.find(params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   # admin未作成のため、商品登録用。後々消します。
   attachment :image
   # ここまで
+  belongs_to :genre
 end
