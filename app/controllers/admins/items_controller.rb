98382b790cb1ed843fe7241f2171f3f4566bc247
panna001 <sonar.pocket.0926@gmail.com>
2021-02-19 07:22:06 +0000

    [Add]admin_items_edit

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -4,28 +4,38 @@ class Admins::ItemsController < ApplicationController
     @items = Item.page(params[:page])
   end
 
-  def create
-    item = Item.new(item_params)
-    item.save
-    redirect_to admins_item_path(item)
-  end
-
-  def edit
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
     @item = Item.new()
   end
 
-  def show
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admins_item_path(item)
+    else
+      render :new
+    end
+  end
+
+  def edit
     @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admins_item_path(@item)
+    else
+      render :update
+    end
   end
 
   private
 
-  # def item_params
-  #   params.require(:item).permit(:name, :image, :caption, :non_tax_price, :genre, :sale_status)
-  # end
   def item_params
     params.require(:item).permit(:name, :image, :caption, :genre_id, :non_tax_price, :sale_status)
   end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,32 +2,12 @@ class ItemsController < ApplicationController
   def index
     @item_all = Item.all
     @items = Item.page(params[:page]).per(8)
-
-# 商品登録がしづらいので、作成。後々消します。
-    @item = Item.new()
-# ここまで
   end
-
-# 商品登録がしづらいので、作成。後々消します。
-  # def create
-  #   item = Item.new(item_params)
-  #   item.save
-  #   redirect_back(fallback_location: :top)
-  # end
-# ここまで
-
 
   def show
     @item = Item.find(params[:id])
     # 不要かもしれない↓
     @cart_item = CartItem.new()
   end
-
-# admin未作成のため、商品登録用。後々消します。
-  private
-  def item_params
-    params.require(:item).permit(:name, :caption, :non_tax_price, :sale_status)
-  end
-# ここまで
 
 end
