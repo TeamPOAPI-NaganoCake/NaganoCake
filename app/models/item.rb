8e3c0fc740fc133a3f166395f0dd12f27eebdc0b
panna001 <sonar.pocket.0926@gmail.com>
2021-02-19 02:22:02 +0000

    [Fix]

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,6 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
+  belongs_to :genre
   attachment :image
 
-<<<<<<< HEAD
-=======
-  # ここまで
-  belongs_to :genre
->>>>>>> d70b648592fdba92fc3d6c66b6c13a44a0088481
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,74 +1,42 @@
 Rails.application.routes.draw do
-  root 'homes#top'
-  get '/about' => 'homes#about'
-
-  devise_for :admins
   namespace :admins do
     resources :customers, only: [:index, :edit, :show, :update]
-    
+
     resources :items, only: [:new, :index, :create, :edit, :show, :update]
-    
+
     resources :genres, only: [:index, :create, :edit, :update]
-   
+
     resources :orders, only: [:show, :update] do
       resources :order_items, only: [:update]
     end
-    
+
     get 'homes/top'
   end
   get '/admin', to: 'admin/homes#top'
-  
+
   devise_for :admins
   get 'customers/edit' => 'customers#edit'
   devise_for :customers
-  get 'customers/edit' => 'customers#edit'
-  get 'customers/my_page' => 'customers#show', as: 'customers'
-
-  delete 'cart_items/destroy_all' => "cart_items#destroy_all", as: "destroy_all"
-  resources :cart_items, only: [:index, :create, :update, :destroy]
-
-  # admin未作成のため、商品登録用に「create」追加してます。後々削除
-  resources :items, only: [:index, :show, :create]
-
-  resources :orders, only: [:new, :index, :show, :create]
+  get 'orders/new'
+  resources :orders, only: [:index, :show, :create]
   get 'orders/confirm'
   get 'orders/thanks'
-
+  delete 'cart_items/destroy_all' => "cart_items#destroy_all", as: "destroy_all"
+  resources :cart_items, only: [:index, :create, :update, :destroy]
+  # admin未作成のため、商品登録用に「create」追加してます。後々削除
+  resources :items, only: [:index, :show, :create]
+  # ↑↑
   resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
   resources :customers, only: [:show, :edit, :update]
   resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
   get 'customers/unsubscribe/:id' => 'customers#unsubscribe',as: 'confirm_unsubscribe'
   patch 'customers/withdrew/:id' => "customers#withdrew", as: 'customer_withdrew'
-
-
-  # =========================以下不要===========================
-
-  # get 'orders/show'
-  # namespace :admins do
-  #   get 'customers/index'
-  #   get 'customers/show'
-  #   get 'customers/edit'
-  # end
-  # namespace :admins do
-  #   get 'items/index'
-  #   get 'items/edit'
-  #   get 'items/new'
-  #   post 'items/create'
-  #   get 'items/show'
-  # end
-  # namespace :admins do
-  #   get 'genres/index'
-  #   get 'genres/edit'
-  # end
-  # namespace :admins do
-  #   get 'homes/top'
-  # end
-  # devise_for :admins
-
-  # get 'customers/edit' => 'customers#edit'
-  # get 'orders/new'
-  # get 'homes/top'
-  # get 'homes/about'
+  get 'homes/top'
+  get 'homes/about'
+  root 'homes#top'
+  get '/about' => 'homes#about'
+  get 'customers/my_page' => 'customers#show', as: 'customers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
+
